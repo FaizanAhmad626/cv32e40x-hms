8363c560d194fe3812d42262024f1a79de90f45c
FaizanAhmad626 <faizanahmadq1@gmail.com>
2026-09-09 22:16:02 +0500

    sw/apps: add SEGGER Embedded Studio build of the LED demo
    Same core configuration as the GNU flow: rv32imc_zicsr_zifencei,
    FLASH1 at 0x80000000 and RAM1 at 0x80008000 inside the 64 KB BRAM.
    trap_entry uses .align 7 so the CV32E40X mtvec[6:2] hardwiring does
    not truncate the vector. Validated on hardware.

---
+++ sw/apps/segger/led_demo/Output/Debug/Obj/led_demo/main.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/led_demo/main.o: \
+ D:\Documents\SideTasks\20260225_Health_Monitoring_System\HMS_Git\cv32e40x-hms\sw\apps\segger\led_demo\Source\main.c
---
+++ sw/apps/segger/led_demo/Output/Debug/Obj/led_demo/riscv_crt0.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/led_demo/riscv_crt0.o: \
+ D:\Documents\SideTasks\20260225_Health_Monitoring_System\HMS_Git\cv32e40x-hms\sw\apps\segger\led_demo\System\riscv_crt0.s
